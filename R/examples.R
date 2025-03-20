edb2c327c0bf3c6a3f8caef02193fa25e01d7b60
Pablo Rodríguez-Sánchez <pablo.rodriguez.sanchez@gmail.com>
2025-03-20 13:02:35 +0100

    Update R/examples.R
    Co-authored-by: Don van den Bergh <donvdbergh@hotmail.com>

--- R/examples.R
+++ R/examples.R
@@ -22,8 +22,8 @@ ProcessTable <- function(jaspResults, dataset, options) {
   # Prints the inputs as a table
   stats <- createJaspTable(gettext("Some descriptives"))
 
-  stats$addColumnInfo(name = "times")
-  stats$addColumnInfo(name = "xs")
+  stats$addColumnInfo(name = gettext("times"))
+  stats$addColumnInfo(name = gettext("xs"))
 
   stats[["times"]] <- dataset[, options$ts]
   stats[["xs"]] <- dataset[, options$xs]
